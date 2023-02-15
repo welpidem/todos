9143d901c8746056260627a4a8a9d55b7c528dd8
Medi Jem Manso <medimanso12@gmail.com>
2023-02-15 10:29:39 +0800

    Update edit-todo-item.vue

--- src/components/edit-todo-item.vue
+++ src/components/edit-todo-item.vue
@@ -36,7 +36,7 @@ const show = (text: string, id: string) => {
     </div>
     <div v-else style="align-items: center; padding: 1% 0 1% 0%;">
         <form @submit.prevent="updateItem(editingItem, todoItemId)">
-            <input type="text" v-model="editingItem" @keyup.enter="updateItem(editingItem, todoItemId), closeEdit()"
+            <i>Edit Task: </i><input type="text" v-model="editingItem" @keyup.enter="updateItem(editingItem, todoItemId), closeEdit()"
                 style="font-size: medium; margin-right: 2%;" />
             <button type="submit" style="margin-right: 1%;">Update</button>
             <button @click="closeEdit()">Cancel</button>
